1983c3a9432dbdc9b6923a374f81062bc8de52ce
Diz111Diz <kavunv@gmail.com>
2020-02-04 21:09:31 +0200

    add hw-08

---
+++ galleryJs.js
@@ -0,0 +1,46 @@
+ 'use strict'
+ import gallery from "./gallery-items.js";
+
+ const galleryList = document.querySelector('.js-gallery');
+ const modalBox = document.querySelector('.lightbox__content');
+ const modelFullImg = document.querySelector('.lightbox__image');
+ const divLightbox = document.querySelector('.lightbox');
+ const btnClose = document.querySelector('.lightbox__button');
+
+ const renderAddItem = ({
+   preview,
+   original,
+   description
+ }) => {
+   return `<li class="gallery__item">
+  <a class="gallery__link"
+    href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}">
+  </a>
+</li>`;
+ };
+
+ const addRenderList = link => link.reduce((acc, el) => acc + renderAddItem(el), '');
+
+ galleryList.insertAdjacentHTML('beforeend', addRenderList(gallery));
+
+
+ galleryList.addEventListener('click', modalOpen);
+
+ function modalOpen(e) {
+   e.preventDefault();
+   modelFullImg.setAttribute('src', e.target.dataset.source);
+   modelFullImg.setAttribute('alt', e.target.alt);
+   divLightbox.classList.add('is-open');
+ };
+
+ btnClose.addEventListener('click', modalClose);
+
+ function modalClose(e) {
+   modelFullImg.setAttribute('src', "");
+   modelFullImg.setAttribute('alt', "");
+   divLightbox.classList.remove('is-open');
+ }
